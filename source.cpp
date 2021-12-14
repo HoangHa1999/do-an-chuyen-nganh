15b3c1836a9c6b9ae7d972da66082fb599f8cc77
HoangHa1999 <ngocha1999.hh@gmail.com>
2021-12-14 20:28:55 +0700

    Thêm source.cpp

---
+++ source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b);
+{
+	return a+b;
+}
+int hieu(int a, int b);
+{
+	return	a-b;
+}
+int tich(int a, int b){
+	return a*b;	
+}
+
+
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap so nguyen 1: ";
+	cin >> a;
+	cout << "Nhap so nguyen 2: ";
+	cin >> b;
+	cout << "Chon phep toan (+,-) ";
+	cin >> phepToan;
+	if(pheptoan == "-")
+	cout<<"Hieu 2 so la"<<hieu(a,b);
+	if(pheptoan == "+")
+	cout<< "Tong 2 so la"<<tong(a,b);
+	system("pause");
+	return 0;
+}
